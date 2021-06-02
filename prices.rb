2a8d5d2a0cf2edc598a856d80feb9e6ee5e3abd3
Rodney Gelera <rodney.gelera@shopify.com>
2021-06-01 22:20:17 -0700

    added method for array

--- prices.rb
+++ prices.rb
@@ -27,7 +27,28 @@ def show_discounts(prices)
   end
 end
 
+def do_something_with_every_item(array, operation)
+  if operation == "total" or operation == "refund"
+    amount = 0
+  end
+  index = 0
+  while index < array.length
+    if operation == "total"
+      amount += array[index]
+    elsif operation == "refund"
+      amount -= array[index]
+    elsif operation == "show discounts"
+      amount_off = array[index] / 3.0
+      puts format("Your discount: $%.2f", amount_off)
+    end
+    index += 1
+  end
+  if operation == "total" or operation == "refund"
+    return amount
+  end
+end
+
 prices = [3.99, 25.00, 8.99]
-puts format("%.2f", total(prices))
-puts format("%.2f", refund(prices))
-show_discounts(prices)
+puts format("%.2f", do_something_with_every_item(prices, "total"))
+puts format("%.2f", do_something_with_every_item(prices, "refund"))
+do_something_with_every_item(prices, "show discounts")
